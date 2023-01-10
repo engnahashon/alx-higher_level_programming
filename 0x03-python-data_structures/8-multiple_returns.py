9bb286adc932cfe59ad8c71521611447b99e47b9
engnahashon <nahashon.ndirangu@outlook.com>
2023-01-10 06:41:11 +0000

    Python Data Structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
-        return (None)
+        return (length, None)
     else:
         return(length, sentence[0])
