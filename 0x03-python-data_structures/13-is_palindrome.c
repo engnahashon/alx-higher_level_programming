81c08595200596d7e4188a8d9f2357ec22988212
engnahashon <nahashon.ndirangu@outlook.com>
2023-01-10 10:07:05 +0000

    Python Data Structures

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+  * is_palindrome - checks if a singly linked list is a palindrome
+  * @head: Head
+  *
+  * Return: 0 if palindrome, 1 if not
+  */
+int is_palindrome(listint_t **head)
+{
+    (void)head;
+
+    return (1);
+}
