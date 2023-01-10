25737aa0482b9ea55eec8bf5cb0d89e4849707e9
engnahashon <nahashon.ndirangu@outlook.com>
2023-01-10 08:34:18 +0000

    Python Data Structures

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-
+    new_list = my_list[:]
+    indx = 0
     for i in my_list:
-        if i / 2 == 0:
-            
+        if i % 2 == 0:
+            new_list[indx] = True
+        else:
+            new_list[indx] = False
+        indx += 1
+    return (new_list)
