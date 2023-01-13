848d6eb586b5ce78385bba4721e8c6ece83ff580
engnahashon <nahashon.ndirangu@outlook.com>
2023-01-13 17:32:38 +0000

    Python more data structures

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square(num):
+    return num ** 2
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        new_matrix.append(list(map(square, i)))
+    return new_matrix
