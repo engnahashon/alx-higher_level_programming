f8141474d5525a3e445fcd59c5879843a7a22c5c
engnahashon <nahashon.ndirangu@outlook.com>
2023-02-14 20:38:43 +0000

    crate

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -3,10 +3,3 @@
 
 -- Create hbtn_0c_0 database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
--- Display a message indicating whether the database was created or not
-SELECT 
-    CASE 
-        WHEN ROW_COUNT() = 0 THEN 'Database already exists'
-        ELSE 'Database created successfully'
-    END AS MESSAGE;
