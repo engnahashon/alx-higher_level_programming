c91a7f984645ec748b0288f5283dfd31e22f95f2
engnahashon <nahashon.ndirangu@outlook.com>
2023-02-15 09:26:32 +0000

    4

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -4,7 +4,4 @@
 USE database_name;
 
 -- Get a list of all tables in the database
-SELECT table_name
-FROM information_schema.tables
-WHERE table_type = 'BASE TABLE'
-AND table_schema = 'database_name';
+SHOW TABLES;
