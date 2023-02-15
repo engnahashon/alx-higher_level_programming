7f33dbf5964c210c5c910264859933485bb26c2f
engnahashon <nahashon.ndirangu@outlook.com>
2023-02-15 20:01:34 +0000

    first add

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,4 +1,3 @@
--- cript that lists all rows of the table first_table from the database hbtn_0c_0
--- All fields should be printed
+-- Script that lists all rows of the table first_table from the database hbtn_0c_0
 
 SELECT * FROM first_table;
