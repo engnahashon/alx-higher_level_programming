270134d49d7a5e22ac0e1fe9851e9b1fdc99e7f2
engnahashon <nahashon.ndirangu@outlook.com>
2023-02-15 20:28:43 +0000

    2

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
 -- Creates the MySQL server user user_0d_1.
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+
+CREATE USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
 FLUSH PRIVILEGES;
