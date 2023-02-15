36c222a7dea9a33b138cf6089dbbffbe5db1e556
engnahashon <nahashon.ndirangu@outlook.com>
2023-02-15 20:24:36 +0000

    up

---
+++ 0x0D-SQL_introduction.bak/3-list_tables.sql
@@ -0,0 +1,7 @@
+-- Script: List tables in a database
+-- Task: List all the tables in a MySQL database
+
+USE database_name;
+
+-- Get a list of all tables in the database
+SHOW TABLES;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,7 @@
 -- a script that creates a table second_table in the database hbtn_0c_0
 -- add multiples rows
 
-CREATE TABLE IF NOT EXISTS second_table
-(
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT,
