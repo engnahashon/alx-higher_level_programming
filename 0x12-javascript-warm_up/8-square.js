e0494b6829446606adcab2ba3b1078289d3994ee
engnahashon <nahashon.ndirangu@outlook.com>
2023-03-16 13:36:42 +0000

    square semistd

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,16 +2,17 @@
 
 const num = process.argv[2];
 if (isNaN(num) || undefined) {
-  console.log('Missing size');} else {
-	  let x = 0;
-	  while (x < num) {
-	  let y = 0;
-	  let row = ''
-	  while (y < num) {
-		  row += 'X'
-		  y++;
-	  }
-	  console.log(row);
-	  x++;
+  console.log('Missing size');
+} else {
+  let x = 0;
+  while (x < num) {
+    let y = 0;
+    let row = '';
+    while (y < num) {
+      row += 'X';
+      y++;
+    }
+    console.log(row);
+    x++;
   }
-  }
+}
