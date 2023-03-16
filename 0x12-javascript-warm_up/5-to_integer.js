6dcd6090c260aa995c7d4367b4621ca288e0eb7b
engnahashon <nahashon.ndirangu@outlook.com>
2023-03-16 12:28:47 +0000

    task 6

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-let num = (process.argv[2]);
+const num = (process.argv[2]);
 
 if (isNaN(num)) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log('My number: ' + num);
+  console.log('My number: ' + num);
 }
-
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const line = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let x = 0;
+while (x < line.length) {
+  console.log(line[x]);
+  x++;
+}
