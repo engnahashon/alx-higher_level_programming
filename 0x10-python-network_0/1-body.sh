e20f78e1ad0330892ab4c50717474c717e0fae60
engnahashon <nahashon.ndirangu@outlook.com>
2023-03-30 12:58:08 +0000

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #  sends a GET request to the URL, and displays the body of the response
-response=$(curl -s -w "%{http_code}" $1)
+response=$(curl -s -w "%{http_code}" "$1")
 
-if [ ${response:(-3)} = "200" ]; then
-    body=$(curl -s $1)
-    echo $body
+if [ "${response:(-3)}" = "200" ]; then
+    body=$(curl -s "$1")
+    echo "$body"
 fi
