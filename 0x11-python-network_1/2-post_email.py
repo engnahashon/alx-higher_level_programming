8c5aad010469292210639e96cadf6290d08f81b7
engnahashon <nahashon.ndirangu@outlook.com>
2023-04-04 09:03:31 +0000

    email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
-#this is a comment
+"""
+Takes in a URL and an email
+sends a POST request to the passed URL with the email as a parameter
+and displays the body of the response
+"""
+import sys
+import urllib.request
+import urllib.parse
 
+
+if __name__ == "__main__":
+    email = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(email)
+    data = data.encode('ascii')
+    request = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(request) as response:
+        html = response.read()
+    print(html.decode())
