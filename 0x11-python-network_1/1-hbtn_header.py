0bd42f7a54975bd665e659b8f6fa0d9c0fd36dc5
engnahashon <nahashon.ndirangu@outlook.com>
2023-04-04 07:33:40 +0000

    header value 0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-#this is a comment
-
+"""displays the value of the X-Request-Id variable"""
+import sys
+import urllib.request
+with urllib.request.urlopen(sys.argv[1]) as response:
+    request_id = response.getheader('X-Request-Id')
+    print(request_id)
