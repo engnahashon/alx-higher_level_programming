19b4b72ab7d05dc0350171dceacd96ecac3ffedf
engnahashon <nahashon.ndirangu@outlook.com>
2023-05-23 14:09:46 +0000

    is same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Python script for task 2"""
+
+
+def is_same_class(obj, a_class):
+    """
+    returns True if the object is exactly an instance of the specified class ; otherwise False
+    """
+    return type(obj) == a_class
