973b518911d99604f08f19be98332868c5deac5d
engnahashon <nahashon.ndirangu@outlook.com>
2023-05-23 14:37:05 +0000

    issubclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script for task 4"""
+
+
+def inherits_from(obj, a_class):
+    """
+    returns True if the object is an instance of a class that
+    inherited from the specified class ; otherwise False
+    """
+    if issubclass(type(obj), a_class):
+        return True
+    else:
+         return False
