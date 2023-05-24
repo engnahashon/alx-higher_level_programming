22c3b47c95fd4b94b5b702abf6e462fff9e757d6
engnahashon <nahashon.ndirangu@outlook.com>
2023-05-24 12:52:18 +0000

    square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python script for task 11"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square that inherits from Rectangle"""
+    def __init__(self, size):
+        """Initiliaze the class attributes"""
+        self.__size = size
+        super().__init__(size, size)
+        self.integer_validator('size', size)
+
+    def area(self):
+        """Implement the area method"""
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """Set custom object representation"""
+        return ("[Square] {}/{}".format(self.__size, self.__size))
