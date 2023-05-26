000582cef3796ef0c0c17fd3468ae05da020ec91
engnahashon <nahashon.ndirangu@outlook.com>
2023-05-26 19:52:14 +0000

    append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Python module for task 1"""
+"""Python module for task 2"""
 
 
 def write_file(filename="", text=""):
     """
-    function that writes a string to a text file (UTF8)
+    function that writes a string at end of text file (UTF8)
     and returns the number of characters written
     """
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'a', encoding='utf-8') as f:
         return (f.write(text))
         
