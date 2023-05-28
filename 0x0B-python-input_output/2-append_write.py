9a05f9fac56269d4936d81598c11daa25306950d
engnahashon <nahashon.ndirangu@outlook.com>
2023-05-28 09:57:57 +0000

    d

--- 0x0B-python-input_output/2-append_write.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Python module for task no 2"""
-
-
-def write_file(filename="", text=""):
-    """function that appends a string at the end of a text file (UTF8)"""
-    with open(filename, 'a', encoding='utf-8') as f:
-        return (f.write(text))
