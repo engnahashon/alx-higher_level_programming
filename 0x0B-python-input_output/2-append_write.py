a6d0ea2359d3b1664072d9c8c3dba2bfa8dab638
engnahashon <nahashon.ndirangu@outlook.com>
2023-05-28 09:47:00 +0000

    2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,7 +3,9 @@
 
 
 def write_file(filename="", text=""):
-    """function that appends a string at the end of a text file"""
+    """
+    function that appends a string at the end of a text file
+    """
     with open(filename, 'a', encoding='utf-8') as f:
         return (f.write(text))
         
