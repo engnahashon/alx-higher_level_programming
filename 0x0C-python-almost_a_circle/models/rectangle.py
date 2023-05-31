90f5e6433e277ec3a43e5a89a21198dba5cfd24d
engnahashon <nahashon.ndirangu@outlook.com>
2023-05-31 08:47:03 +0000

    validate

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,10 +7,10 @@ class Rectangle(Base):
     """class Rectangle that inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Class Constructor"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
@@ -60,7 +60,7 @@ class Rectangle(Base):
     def y(self, value):
         if not isinstance(value, int):
             raise TypeError('y must be an integer')
-        elif value < 0:
+        if value < 0:
             raise ValueError('y must be >= 0')
         else:
             self.__y = value
