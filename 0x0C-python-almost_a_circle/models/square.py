d68899fa30bdb72e2e73e9324e3f3c3b55eb502c
engnahashon <nahashon.ndirangu@outlook.com>
2023-06-02 08:05:30 +0000

    create square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python model for Square, almost a circle task"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class Square that inherits from Rectangle"""
+    
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Class constructor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Overloading the __str__"""
+        return('[Square] ({}) {}/{} - {}'.format(
+                self.id, self.x, self.y, self.width))
