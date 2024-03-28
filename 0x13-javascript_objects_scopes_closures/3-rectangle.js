63a4a96b6d3a7a2b27185648d99c816c8dfd974a
engnahashon <nahashon.ndirangu@outlook.com>
2024-03-28 12:17:57 +0000

    print

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w === undefined || h === undefined || w <= 0 || h <= 0) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let x = 1; x <= this.height; x++) {
+      let rect = '';
+      for (let y = 1; y <= this.width; y++) {
+        rect += 'x';
+      }
+      console.log(rect);
+    }
+  }
+}
+module.exports = Rectangle;
