f59ffc7e76fd728870208a23d66b21a72fab1db8
engnahashon <nahashon.ndirangu@outlook.com>
2024-03-29 11:13:43 +0000

    converter

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-    
-}
+  return function convertToBase10 (number) {
+    return number < base ? String(number) : convertToBase10(Math.floor(number / base)) + String(number % base);
+  };
+};
